b431a558688657ff39040357e8c4b5a11d23ecce
Joe Barnes <barnesjd@gmail.com>
2014-03-07 10:01:33 -0600

    Started PeanoVal, but tests hang

---
+++ src/main/scala/com/joescii/typed/PeanoVal.scala
@@ -0,0 +1,12 @@
+package com.joescii.typed
+
+sealed trait PeanoVal {
+  def le(that:PeanoVal):BoolVal
+}
+
+case object Val0 extends PeanoVal {
+  override def le(that:PeanoVal) = TrueVal
+}
+case class ValN(prev:PeanoVal) extends PeanoVal {
+  override def le(that:PeanoVal) = TrueVal
+}
---
+++ src/test/scala/com/joescii/typed/PeanoValSpecs.scala
@@ -0,0 +1,20 @@
+package com.joescii.typed
+
+import org.scalatest._
+import matchers.ShouldMatchers
+
+class PeanoValSpecs extends WordSpec with ShouldMatchers {
+  "Val0" should {
+    "be less than or equal to itself" in { (Val0 le Val0 ) should be (TrueVal) }
+//    "not equal val1" in { val1 should not equal (Val0) }
+//    "be less than or equal to ValN(Val0)" in { (Val0.le(ValN(Val0))) should be (TrueVal) }
+  }
+
+//  "Val1 = ValN(Val0)" should {
+//    "run" in { ValN(Val0) should not be (null) }
+//    val Val1 = ValN(Val0)
+//    "not be less than or equal to Val0" in { (Val1 le Val0) should be (FalseVal) }
+//    "be less than or equal to itself" in { (Val1 le Val1) should be (TrueVal) }
+//    "be less than or equal to ValN(ValN(Val0))" in { (Val1 le ValN(Val1)) should be (TrueVal) }
+//  }
+}
