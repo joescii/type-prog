cbde340b1305d4050804124fe4fecca21444dce1
Joe Barnes <barnesjd@gmail.com>
2014-03-16 13:06:42 -0500

    Implemented an UnsafeIntList

---
+++ src/main/scala/com/joescii/typed/UnsafeIntList.scala
@@ -0,0 +1,25 @@
+package com.joescii.typed
+
+sealed trait UnsafeIntList { self =>
+  def ::(head:Int):UnsafeIntList = UnsafeIntListImpl(head, self)
+  def +(other:UnsafeIntList):UnsafeIntList
+  def size:Int 
+}
+
+case object UnsafeIntNil extends UnsafeIntList { self =>
+  override def +(other:UnsafeIntList) = {
+    require(other == UnsafeIntNil, "argument 'other' must be the UnsafeIntNil object")
+    self:UnsafeIntList
+  }
+  override val size = 0
+}
+
+private [typed] case class UnsafeIntListImpl(head:Int, tail:UnsafeIntList) extends UnsafeIntList {
+  override def +(other:UnsafeIntList) = {
+    require(other.size == size, "argument 'other' must have the same size as this UnsafeIntList")
+    other match {
+      case UnsafeIntListImpl(h, t) => (head + h) :: (tail + t)
+    }
+  }
+  override val size = 1 + tail.size 
+}
---
+++ src/test/scala/com/joescii/typed/UnsafeIntListSpecs.scala
@@ -0,0 +1,44 @@
+package com.joescii.typed
+
+import org.scalatest._
+import matchers.ShouldMatchers
+
+class UnsafeIntListSpecs extends FlatSpec with ShouldMatchers {
+//  illTyped("val nil:IntNil = 1 :: IntNil")
+//  illTyped("(1 :: IntNil) + IntNil")
+//  illTyped("(1 :: 2 :: IntNil) + IntNil")
+//  illTyped("(1 :: 2 :: IntNil) + (3 :: IntNil)")
+//  illTyped("IntNil + (3 :: IntNil)")
+
+  "The UnsafeIntNil" should "equal itself under addition" in {
+    val sum = UnsafeIntNil + UnsafeIntNil
+    sum should equal (UnsafeIntNil)
+  }
+
+  "1 :: UnsafeIntNil + 2 :: UnsafeIntNil" should "equal 3 :: UnsafeIntNil" in {
+    val sum = (1 :: UnsafeIntNil) + (2 :: UnsafeIntNil)
+    sum should equal (3 :: UnsafeIntNil)
+  }
+
+  "1 :: 2 :: UnsafeIntNil + 3 :: 4 :: UnsafeIntNil" should "equal 4 :: 6 :: UnsafeIntNil" in {
+    val sum = (1 :: 2 :: UnsafeIntNil) + (3 :: 4 :: UnsafeIntNil)
+    sum should equal (4 :: 6 :: UnsafeIntNil)
+  }
+
+  "1 :: UnsafeIntNil + UnsafeIntNil" should "throw an exception" in {
+    intercept[IllegalArgumentException]((1 :: UnsafeIntNil) + UnsafeIntNil)
+  }
+
+  "1 :: 2 :: UnsafeIntNil + UnsafeIntNil" should "throw an exception" in {
+    intercept[IllegalArgumentException]((1 :: 2 :: UnsafeIntNil) + UnsafeIntNil)
+  }
+
+  "1 :: 2 :: UnsafeIntNil + 3 :: UnsafeIntNil" should "throw an exception" in {
+    intercept[IllegalArgumentException]((1 :: 2 :: UnsafeIntNil) + (3 :: UnsafeIntNil))
+  }
+
+  "UnsafeIntNil + 3 :: UnsafeIntNil" should "throw an exception" in {
+    intercept[IllegalArgumentException]((UnsafeIntNil) + (3 :: UnsafeIntNil))
+  }
+
+}
