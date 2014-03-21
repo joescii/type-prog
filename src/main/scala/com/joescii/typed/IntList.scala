d40cc25157471b0f2fadf6b956612d95f90f6970
Joe Barnes <barnesjd@gmail.com>
2014-03-21 18:12:37 -0500

    Changing IntList to use this/that instead of self/other

--- src/main/scala/com/joescii/typed/IntList.scala
+++ src/main/scala/com/joescii/typed/IntList.scala
@@ -2,21 +2,21 @@ package com.joescii.typed
 
 sealed trait IntList[Size <: SizeType] {
   def ::(head:Int):IntList[SizeN[Size]] = IntListImpl(head, this)
-  def +(other:IntList[Size]):IntList[Size]
-  def ++[OtherSize <: SizeType](other:IntList[OtherSize]):IntList[Size#plus[OtherSize]]
+  def +(that:IntList[Size]):IntList[Size]
+  def ++[ThatSize <: SizeType](that:IntList[ThatSize]):IntList[Size#plus[ThatSize]]
   def size:Int
 }
 
 case object IntNil extends IntList[Size0] {
-  override def +(other:IntList[Size0]) = this
+  override def +(that:IntList[Size0]) = this
   override val size = 0
-  override def ++[OtherSize <: SizeType](other:IntList[OtherSize]) = other
+  override def ++[ThatSize <: SizeType](that:IntList[ThatSize]) = that
 }
 private[typed] case class IntListImpl[SizeTail <: SizeType](head:Int, tail:IntList[SizeTail]) extends IntList[SizeN[SizeTail]] { 
   private type Size = SizeN[SizeTail] // defined for clarity
-  override def +(other:IntList[Size]) = other match {
+  override def +(that:IntList[Size]) = that match {
     case IntListImpl(h, t) => (head + h) :: (tail + t)
   }
   override val size = 1 + tail.size
-  override def ++[OtherSize <: SizeType](other:IntList[OtherSize]) = IntListImpl(head, tail++other)
+  override def ++[ThatSize <: SizeType](that:IntList[ThatSize]) = IntListImpl(head, tail++that)
 }
