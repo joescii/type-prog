811d1d563b04d2a9f825ae9b1ddde8e3daf60188
Joe Barnes <barnesjd@gmail.com>
2014-03-20 21:45:43 -0500

    Implemented IntList ++

--- src/main/scala/com/joescii/typed/IntList.scala
+++ src/main/scala/com/joescii/typed/IntList.scala
@@ -3,12 +3,14 @@ package com.joescii.typed
 sealed trait IntList[Size <: SizeType] { self =>
   def ::(head:Int):IntList[SizeN[Size]] = IntListImpl(head, self)
   def +(other:IntList[Size]):IntList[Size]
+  def ++[OtherSize <: SizeType](other:IntList[OtherSize]):IntList[Size#add[OtherSize]]
   def size:Int
 }
 
 case object IntNil extends IntList[Size0] { self =>
   override def +(other:IntList[Size0]) = self
   override val size = 0
+  override def ++[OtherSize <: SizeType](other:IntList[OtherSize]) = other
 }
 private[typed] case class IntListImpl[SizeTail <: SizeType](head:Int, tail:IntList[SizeTail]) extends IntList[SizeN[SizeTail]] { self =>
   private type Size = SizeN[SizeTail] // defined for clarity
@@ -16,4 +18,5 @@ private[typed] case class IntListImpl[SizeTail <: SizeType](head:Int, tail:IntLi
     case IntListImpl(h, t) => (head + h) :: (tail + t)
   }
   override val size = 1 + tail.size
+  override def ++[OtherSize <: SizeType](other:IntList[OtherSize]) = IntListImpl(head, tail++other)
 }
--- src/test/scala/com/joescii/typed/IntListSpecs.scala
+++ src/test/scala/com/joescii/typed/IntListSpecs.scala
@@ -25,4 +25,29 @@ class IntListSpecs extends FlatSpec with ShouldMatchers {
     val sum = (1 :: 2 :: IntNil) + (3 :: 4 :: IntNil)
     sum should equal (4 :: 6 :: IntNil)
   }
+
+  "The IntNil" should "equal itself under ++" in {
+    val list = IntNil ++ IntNil
+    list should equal (IntNil)
+  }
+
+  "1 :: IntNil ++ IntNil" should "equal 1 :: IntNil" in {
+    val list = (1 :: IntNil) ++ IntNil
+    list should equal(1 :: IntNil)
+  }
+
+  "1 :: IntNil ++ 2 :: IntNil" should "equal 1 :: 2 :: IntNil" in {
+    val list = (1 :: IntNil) ++ (2 :: IntNil)
+    list should equal (1 :: 2 :: IntNil)
+  }
+
+  "1 :: 2 :: IntNil ++ 3 :: 4 :: IntNil" should "equal 1 :: 2 :: 3 :: 4 :: IntNil" in {
+    val list = (1 :: 2 :: IntNil) ++ (3 :: 4 :: IntNil)
+    list should equal (1 :: 2 :: 3 :: 4 :: IntNil)
+  }
+
+  "(1 :: 2 :: IntNil ++ 3 :: IntNil) + (4 :: 5 :: 6 :: IntNil)" should "equal 5 :: 7 :: 9 :: IntNil" in {
+    val sum = ((1 :: 2 :: IntNil) ++ (3 :: IntNil)) + (4 :: 5 :: 6 :: IntNil)
+    sum should equal (5 :: 7 :: 9 :: IntNil)
+  }
 }
